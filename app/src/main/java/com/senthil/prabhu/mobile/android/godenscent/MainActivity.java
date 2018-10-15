2c8d858c10abba0719892c5689ba9c0a6689c8d4
Senthil Prabhu Bose <bosesenthilprabhu@gmail.com>
2018-10-15 20:31:17 +0400

    Initial Project

---
+++ app/src/main/java/com/senthil/prabhu/mobile/android/godenscent/MainActivity.java
@@ -0,0 +1,13 @@
+package com.senthil.prabhu.mobile.android.godenscent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
